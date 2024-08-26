ec17da7a21fc5e32d2eac39ade4e518dd51a00cf
MATEUS MOREIRA FONSECA <mateus.fonseca1992@gmail.com>
2024-08-26 11:31:09 -0300

    Rollback

--- aplicacao3/backendAplicacao3/models/User.js
+++ aplicacao3/backendAplicacao3/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const saltRounds = 10; // O número de rounds para o salt
 
 const userSchema = new Schema({
   username: {
@@ -20,11 +20,14 @@ const userSchema = new Schema({
   },
 });
 
+// Middleware para criptografar a senha antes de salvar o usuário
 userSchema.pre('save', function (next) {
   const user = this;
 
+  // Apenas criptografa a senha se ela foi modificada ou é nova
   if (!user.isModified('senha')) return next();
 
+  // Gera um salt e usa-o para criptografar a senha
   bcrypt.hash(user.senha, saltRounds, function (err, hash) {
     if (err) return next(err);
     user.senha = hash;
@@ -32,6 +35,7 @@ userSchema.pre('save', function (next) {
   });
 });
 
+// Método para comparar a senha fornecida com a senha criptografada
 userSchema.methods.comparePassword = function (candidatePassword, callback) {
   bcrypt.compare(candidatePassword, this.senha, function (err, isMatch) {
     if (err) return callback(err);
